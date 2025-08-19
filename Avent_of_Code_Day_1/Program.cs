9ff700954ef91751508566fe63a47223697770a8
Vusumzi Vincent <vusumziz@geotab.co.za>
2025-08-19 19:09:08 +0200

    Implemented part two code challenge

--- Avent_of_Code_Day_1/Program.cs
+++ Avent_of_Code_Day_1/Program.cs
@@ -9,25 +9,17 @@ namespace Avent_of_Code_Day_1
 
 
         {
-            string? inputTextLine = String.Empty;
-            int[] locationId_group1 = [];
-            int[] locationId_group2 = [];
+            var (locationId_group1,locationId_group2) = GetInputData();
 
-           
-            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
-            StreamReader sr = new StreamReader(inputFilePath);
+            PartOne(locationId_group1, locationId_group2);
+            PartTwo(locationId_group1, locationId_group2);
 
-            inputTextLine = sr.ReadLine();
+        }
 
-            while (inputTextLine != null)
-            {
-                var parts = SplitBySpaceCharacter(inputTextLine);
-                locationId_group1 = locationId_group1.Append(parts[0]).ToArray();
-                locationId_group2 = locationId_group2.Append(parts[1]).ToArray();
-                inputTextLine = sr.ReadLine();
-            }
+      
 
-           
+        public static void PartOne(int[]locationId_group1, int[]locationId_group2)
+        {
             int[] distance = [];
             // Sort by ascending order
             int[] sortedLocationId_group1 = locationId_group1.OrderBy(x => x).ToArray();
@@ -43,13 +35,45 @@ namespace Avent_of_Code_Day_1
                     difference = sortedLocationId_group2[i] - sortedLocationId_group1[i];
                 distance = distance.Append(difference).ToArray();
             }
-            Console.WriteLine("Distances between the two groups is: {0}", distance.Sum());
+            Console.WriteLine("Part One => Distances between the two groups is: {0}", distance.Sum());
         }
 
-        public static int[] SplitBySpaceCharacter(string input)
+
+        public static void PartTwo(int[] locationId_group1, int[] locationId_group2)
         {
-            string[] parts = Regex.Split(input, @"\s{3}");
-            return Array.ConvertAll(parts, int.Parse);
+            int[] similarityScore = [];
+            foreach (var locationId in locationId_group1)
+            {
+                var occurence = locationId_group2.Count(x => x == locationId);
+                similarityScore = similarityScore.Append(locationId * occurence).ToArray();
+            }
+
+            Console.WriteLine("Part Two => The similarity score is: {0}", similarityScore.Sum());
         }
+
+
+        public static (int[], int[]) GetInputData()
+        {
+            string? inputTextLine = String.Empty;
+            int[] locationId_group1 = [];
+            int[] locationId_group2 = [];
+
+            string inputFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+            StreamReader sr = new StreamReader(inputFilePath);
+
+            inputTextLine = sr.ReadLine();
+
+            while (inputTextLine != null)
+            {
+                string[] splitValues = Regex.Split(inputTextLine, @"\s{3}");
+                var parts = Array.ConvertAll(splitValues, int.Parse);
+                locationId_group1 = locationId_group1.Append(parts[0]).ToArray();
+                locationId_group2 = locationId_group2.Append(parts[1]).ToArray();
+                inputTextLine = sr.ReadLine();
+            }
+            return (locationId_group1, locationId_group2);
+        }
+
+
     }
 }
